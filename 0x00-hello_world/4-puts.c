6f6f1b55a638074927dd80f5d559311ae47602f7
Bishoy Saeed <bishoysfayez@gmail.com>
2023-08-10 11:06:58 -0700

    file  chmod 4 betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
-/*
- * program that prints " Programming is like building a multilingual puzzle" using puts
- *
+/**
+ * main-Entry point
+ * Description: 'the program prints Programming is like bulding puzzle '
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
