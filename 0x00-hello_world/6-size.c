0fdd898c7c2007f83f91eb59be5a36661ed5cba7
Bishoy Saeed <bishoysfayez@gmail.com>
2023-08-10 11:32:22 -0700

    add 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 /**
  * main-Entry point
- * Description: program prits sizes of different types of c variables 
+ * Description: program prits sizes of different types of c variables
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("Size of char : %i byet(s)\n", sizeof(char));
-	printf("Size of int : %i byet(s)\n", sizeof(int));
-	printf("Size of long int is : %i byet(s)\n", sizeof(long int));
-	printtf("Size of long long is : %i byet(s)\n", sizeof(long long));
-	printf("Size of flaot is : %i byet(s)\n", sizeof(float));
+	printf("Size of char : %i byte(s)\n", sizeof(char));
+	printf("Size of int : %i byte(s)\n", sizeof(int));
+	printf("Size of long int is : %i byte(s)\n", sizeof(long int));
+	printf("Size of long long is : %i byte(s)\n", sizeof(long long));
+	printf("Size of flaot is : %i byte(s)\n", sizeof(float));
 	return (0);
 }
