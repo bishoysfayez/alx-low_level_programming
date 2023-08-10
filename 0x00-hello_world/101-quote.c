9c8d27e4638f3d17ea9492209647b2ecf269ab9c
Bishoy Saeed <bishoysfayez@gmail.com>
2023-08-10 12:09:03 -0700

    100

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,66 @@
+/**
+ * main-Entry point
+ * Description: 'the program prints the following quote
+ * and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar('a');
+        putchar('n');
+    	putchar('d');
+    	putchar(' ');
+    	putchar('t');
+    	putchar('h');
+    	putchar('a');
+    	putchar('t');
+    	putchar('p');
+    	putchar('i');
+    	putchar('e');
+    	putchar('c');
+    	putchar('e');
+    	putchar(' ');
+    	putchar('o');
+    	putchar('f');
+    	putchar(' ');
+    	putchar('a');
+    	putchar('r');
+    	putchar('t');
+    	putchar(' ');
+    	putchar('i');
+    	putchar('s');
+   	putchar(' ');
+    	putchar('u');
+    	putchar('s');
+    	putchar('e');
+    	putchar('f');
+    	putchar('u');
+    	putchar('l');
+    	putchar(34);
+    	putchar(' ');
+    	putchar(45);
+    	putchar(' ');
+    	putchar('D');
+    	putchar('o');
+    	putchar('r');
+    	putchar('a');
+    	putchar(' ');
+    	putchar('K');
+    	putchar('o');
+    	putchar('r');
+    	putchar('p');
+    	putchar('a');
+    	putchar('r');
+    	putchar(44);
+    	putchar(' ');
+    	putchar(50);
+    	putchar(48);
+    	putchar(45);
+    	putchar(49);
+    	putchar(48);
+    	putchar(45);
+    	putchar(49);
+   	putchar(57);
+   	putchar('\n');    
+	return (1);
+}
