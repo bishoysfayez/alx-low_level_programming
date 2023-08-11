1bb46c8a5f3b724534c39fab5d623fc7601a89c8
Bishoy Saeed <bishoysfayez@gmail.com>
2023-08-11 10:38:09 -0700

    file 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main-Entry point
+ * Description: prints 2 digit num comb with comma separator
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 56; i++)
+	{
+		for (j = i + 1; j <= 57; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != 56 && j != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
