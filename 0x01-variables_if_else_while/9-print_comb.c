df6fb01b7dcb4330e5ed42a83893c13ecb83bd4c
Bishoy Saeed <bishoysfayez@gmail.com>
2023-08-11 09:49:45 -0700

    file 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main-Entry point
+ * Description: prints digitswith comma separator
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 57; i++)
+	{
+		putchar(i)
+		putchar(44);
+		putchar(' ');
+	}
+	putchar(57);
+	return (0);
+}
