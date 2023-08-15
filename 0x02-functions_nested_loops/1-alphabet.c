24f81be88d1e7f37dfcee9db6ef0b4c1fcd7a7f8
Bishoy Saeed <bishoysfayez@gmail.com>
2023-08-15 11:29:00 -0700

    add 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ * main-Entry point
+ * Description: prints putchar
+ * Return: Always 0 (Success)
+ */
+int print_alphabet(void)
+
+{
+	char i;
+	for(i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
