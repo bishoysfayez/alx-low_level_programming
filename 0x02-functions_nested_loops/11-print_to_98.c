4ce86fc2fe929e76170130e73cc9f8b0657b1247
Bishoy Saeed <bishoysfayez@gmail.com>
2023-08-15 14:58:47 -0700

    add 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98-Entry point
+ * Description: print nums to 98 starting from n
+ * @a : int to be start point
+ * @b : the seconf int
+ * Return: sum a + b
+ */
+
+void print_to_98(int n)
+{
+	int counter = 0;
+	printf("%i, ", n);
+	while (n < 98)
+	{
+		printf("%i", %n)
+
+			if (counter != n - 1)
+			{
+				ptintf(", ");
+			}
+			else if(counter == n - 1)
+			{
+				printf("\n");
+			}
+	}
+}				
