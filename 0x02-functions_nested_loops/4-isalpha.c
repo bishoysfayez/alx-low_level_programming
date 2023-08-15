3f99bb494192cae1177ab39f7c3c4f4ec5e90e35
Bishoy Saeed <bishoysfayez@gmail.com>
2023-08-15 12:04:44 -0700

    3,4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _islower-Entry point
- * Description: prints if a char is a letter.
+ * _isalpha-Entry point
+ * Description: prints if an int c is a letter.
  * Return: 0 if c is lowercase, 0 if otherwise
  */
-int _islower(int c)
+int _isalpha(int c)
 {
 	if (c >= 65 && c <= 90)
 	{
