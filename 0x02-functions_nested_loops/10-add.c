d86f8cea73cc48bce2f5ef278add9d5b13c87f27
Bishoy Saeed <bishoysfayez@gmail.com>
2023-08-15 13:56:08 -0700

    add 9 modi

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add-Entry point
+ * Description: returns sum of 2 nums
+ * @a : int to be added to second int
+ * @b : the seconf int
+ * Return: sum a + b
+ 
+ int add(int a, int b)
+{
+	return (a + b);
+
+}
