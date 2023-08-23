b9d1cd5dc271c4f9ac914344a3acf1a7f35e462b
Bishoy Saeed <bishoysfayez@gmail.com>
2023-08-23 10:53:23 -0700

    add 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,5 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
+
 /**
  *  _strcat-Entry point
  *  Description:  concatenates two strings
@@ -8,11 +7,15 @@
  *  @dest: A pointer to the string to be concatenated upon.
  *  @src: The source string to be appended to @dest.
  *  Return: char * dest (Success)
+ *  function _strlen
+ *  Description : finds length of a string
+ *  @s: String to get its len
+ *  Return : int length of strin
  */
-
+int _strlen(char *s);
 char *_strcat(char *dest, char *src)
 {
-	char new_string[strlen(dest) + strlen(src) - 1];
+	char new_string[_strlen(dest) + _strlen(src) - 1];
 	int i = 0;
 	int j = 0;
 
@@ -30,4 +33,25 @@ char *_strcat(char *dest, char *src)
 	new_string[i] = '\0';
 	strcpy(dest, new_string);
 	return (dest);
+}
+
+
+/*
+ * Description : finds length of a string
+ * @s: String to get its len
+ * Return : int length of string
+ */
+
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	length++
+
+	return (length)
 }
