86091823ff536453f67309c249e86e7aea65df09
Bishoy Saeed <bishoysfayez@gmail.com>
2023-08-24 12:38:43 -0700

    add3 mod

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,11 @@
  *  Description:  compares 2 strings
  *  @s1: first string
  *  @s2: second string
+ *  @s: String whose length we will determine
  *  Return: int(Sucess) 0 if not same strings, 1 if same strings
+ *  0, if the s1 and s2 are equal;
+ *  a negative value if s1 is less than s2;
+*   a positive value if s1 is greater than s2.
  */
 int _strlen(char *s);
 int _strcmp(char *s1, char *s2)
@@ -19,20 +23,27 @@ int _strcmp(char *s1, char *s2)
 			if (s1[i] != s2[i])
 			{
 				return (0);
+			} 
+			else if (s1[i] > s2[i])
+			{
+				return (1);
+			}
+			else
+			{
+				return (-1);
 			}
 		}
 		return (1);
 
 	}
-	else
+	else if (_strlen(s1) > _strlen(s2))
 	{
-		return (0);
+		return (1);
 	}
 }
 
 /**
  * _strlen- returns the length of a string
- * @s: String whose length we will determine
  * Return: Length of s
  */
 
