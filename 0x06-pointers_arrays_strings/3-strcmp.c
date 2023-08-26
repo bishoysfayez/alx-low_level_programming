c2afcad87c36f82c516c1b332fedb600d7608a5d
Bishoy Saeed <bishoysfayez@gmail.com>
2023-08-26 18:22:01 +0300

    add from new ubu

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,50 +5,20 @@
  *  Description:  compares 2 strings
  *  @s1: first string
  *  @s2: second string
- *  @s: String whose length we will determine
  *  Return: int(Sucess) 0 if not same strings, 1 if same strings
  *  0, if the s1 and s2 are equal;
  *  a negative value if s1 is less than s2;
-*   a positive value if s1 is greater than s2.
+ *   a positive value if s1 is greater than s2.
  */
-int _strlen(char *s);
 int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	if (_strlen(s1) == _strlen(s2))
+	while (s1[i] != '\0')
 	{
-		for (i = 0; i < _strlen(s1); i++)
+		if (s1[i] != s2[i])
 		{
-			if (s1[i] != s2[i])
-			{
-				return (0);
-			} 
-			else if (s1[i] > s2[i])
-			{
-				return (1);
-			}
-			else
-			{
-				return (-1);
-			}
+			return (s1[i] - s2[i]);
 		}
 	}
-}
-
-/**
- * _strlen- returns the length of a string
- * Return: Length of s
- */
-
-int _strlen(char *s)
-{
-	int length = 0;
-
-	while (s[length] != '\0')
-	{
-		length++;
-	}
-	length++;
-	return (length);
 }
