5a0dae3f3d6a5b4306c8f150c0e3338ca5e567ad
Bishoy Saeed <bishoysfayez@gmail.com>
2023-08-30 19:18:20 +0300

    add 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,11 +1,17 @@
 #include "main.h"
 
 /**
- *  _strncat-Entry point
- *  Description:  concatenates two strings
- *  to the end of the string pointed to by @dest.
- *  @dest: A pointer to the string to be concatenated upon.
- *  @src: The source string to be appended to @dest
- *  @n : int of num to stop concat 
- *  Return: char * dest (Sucess)
- */
+*  _print_rev_recursion-Entry point
+*  Description:  prints string reversed.
+*  @s : string to be printed
+*  Return: void (Sucess)
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
