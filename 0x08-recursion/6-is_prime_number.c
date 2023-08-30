3a57706c8060d476599c3557793c4c7b6b295921
Bishoy Saeed <bishoysfayez@gmail.com>
2023-08-30 20:10:40 +0300

    add 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * accpets_division-Entry Point.
+ * @x : integer to check if it accepts divion over
+ * @divisor : the int to check on.
+ * Return: int value 0 if accept division -- 1 if not.
+ */
+int accpets_division(int x, int divisor)
+{
+	if (x % divisor == 0)
+	{
+		return (0);
+	}
+	else if (divisor == x / 2)
+	{
+		return (1);
+	}
+	return (accpets_division(x, divisor - 1));
+}
+
+/**
+ * is_prime_number-Entry Point.
+ * @n : integer to check if it is prime or not
+ * Return: int value 1 if prime -- 0 if not.
+ */
+int is_prime_number(int n)
+{
+	/**
+	 * start with divisor 2
+	 */
+	int divisor = 2;
+
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else if (n <= 3)
+	{
+		return (1);
+	}
+	return (accpets_division(n, divisor));
+}
